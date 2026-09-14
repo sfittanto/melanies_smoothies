9db396aa24f7e8cbaca164938dcf8d163c34f38d
sfittanto <sherri.fittanto@snowflake.com>
2026-09-14 15:42:56 -0500

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SnIS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
 st.write("Orders that need to be filled.")
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 my_dataframe = (
     session.table("SMOOTHIES.PUBLIC.ORDERS")
